57d37002b9737018710784d02ae1d2771d0e92cd
wiwatPER <wiwat37889@gmail.com>
2016-05-23 11:41:29 +0700

    เคลื่อนย้ายจากหน้าหนึ่งไปหน้าหนึ่ง

--- app/src/main/java/pbru/permsook/wiwat/bookroompbru/MainActivity1.java
+++ app/src/main/java/pbru/permsook/wiwat/bookroompbru/MainActivity1.java
@@ -1,7 +1,9 @@
 package pbru.permsook.wiwat.bookroompbru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity1 extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
+    } //method
+
+    public void clickSingUpMain(View view) {
+        startActivity(new Intent(MainActivity1.this,SingUpActivity2.class)); //เคลื่อนย้ายจากหน้าหนึ่งไปหน้าหนึ่ง
     }
-}
+}   // Class
+
---
+++ app/src/main/java/pbru/permsook/wiwat/bookroompbru/SingUpActivity2.java
@@ -0,0 +1,13 @@
+package pbru.permsook.wiwat.bookroompbru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingUpActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up2);
+    }
+}
